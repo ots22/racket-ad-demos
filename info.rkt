77b1286da6faab0ed657ce2fcdcb38a8e6bbde9e
Oliver Strickson <ostrickson@turing.ac.uk>
2020-02-04 09:47:23 +0000

    Add info.rkt

---
+++ info.rkt
@@ -0,0 +1,6 @@
+#lang info
+(define collection "rackpropagator")
+(define deps '("base" "rackunit-lib" "slideshow" "slideshow-repl"))
+(define pkg-desc "Exploring Automatic Differentiation with Racket")
+(define version "0.0")
+(define pkg-authors '(ots22))
