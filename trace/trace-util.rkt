f8a55753a5dbc075a985ef1bdc845b72edca0ff7
Oliver Strickson <ostrickson@turing.ac.uk>
2020-05-09 20:49:04 +0100

    Add trace-e

--- trace/trace-util.rkt
+++ trace/trace-util.rkt
@@ -1,10 +1,12 @@
 #lang racket
 
 (provide cons->trace
-         trace->cons)
+         trace->cons
+         trace-e)
 
 (require "trace.rkt"
-         "trace-core.rkt")
+         "trace-core.rkt"
+         "let-traced.rkt")
 
 (define (cons->trace x)
   (cond
@@ -13,9 +15,18 @@
     [(trace? x) x]
     [else       (val->trace x)]))
 
+
 (define (trace->cons tr)
   (cond
     [(null? (top-val tr)) null]
     [(pair? (top-val tr)) (cons (trace->cons (app& car& tr))
                                 (trace->cons (app& cdr& tr)))]
     [else tr]))
+
+(define (trace-e tr)
+  (cond
+    [(null? (top-val tr)) null]
+    [(pair? (top-val tr)) (cons (traced (car& tr))
+                                (trace-e (traced (cdr& tr))))]
+    [else tr]))
+                                          
